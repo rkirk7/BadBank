2b2e1cadab7952f54873b7391361e826b27a6dea
rkirk7 <129214148+rkirk7@users.noreply.github.com>
2024-04-15 11:14:06 -0400

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -75,7 +75,7 @@ function CreateAccount(){
             Email Address<br/>
             <input type="input" className="form-control" id="email" placeholder="Enter Email Address" value={email} onChange={e => setEmail(e.currentTarget.value)} /> <br />
             Password<br/>
-            <input type="password" className="form-control" id="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.currentTarget.value)} /> <br />
+            <input type="password" className="form-control" id="password" placeholder="Enter Password (at least 8 characters)" value={password} onChange={e => setPassword(e.currentTarget.value)} /> <br />
             <button id="submit" type="submit" className="btn btn-light" onClick={handleCreate} disabled={!formFilled}>Create Account</button> <br />
         </>
         ) : (
