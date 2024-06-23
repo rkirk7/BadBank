3ca18fe01b2c74ef3c91fa2bcecfc64f90c016a7
rkirk7 <129214148+rkirk7@users.noreply.github.com>
2024-06-23 00:33:19 -0400

    still trying to deploy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-var dal = require('./backend/dal.js');
+var dal = require('./dal.js');
 
 const app = express();
 const port = 5000;
