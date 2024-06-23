92834e7fc19375f8d419c3ab5fd69a514153aefd
rkirk7 <129214148+rkirk7@users.noreply.github.com>
2024-06-23 02:14:49 -0400

    mongo

--- backend/dal.js
+++ backend/dal.js
@@ -22,24 +22,13 @@ const client = new MongoClient(uri, {
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      db = client.db("myproject");
     } finally {
       // Ensures that the client will close when you finish/error
       await client.close();
     }
   }
   run().catch(console.dir);
-
-
-
-MongoClient.connect(uri, {tls: true})
-.then((client) => {
-   db = client.db('myproject');
-   console.log('connected to MongoDB');
-   console.log('db:', db);
-})
-.catch(err => {
-  console.error('failed to connect to MongoDB', err);
-});
 
 const firebaseConfig = {
     apiKey: "AIzaSyDhKNCusOPW2y52bMwLnOrXIy-u1y1Q4KI",
