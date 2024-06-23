b80a27c14b0b761e8a44d01d0009f9878cfc362e
rkirk7 <129214148+rkirk7@users.noreply.github.com>
2024-06-23 00:02:24 -0400

    trying to deploy

--- public/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Login from './Components/login'; // Assuming Withdraw component is located in './Components/Withdraw'
-
-test('Bad Bank', () => {
-  render(<Login />);
-  const welcomeText = screen.getByText("Log In");
-  expect(welcomeText).toBeInTheDocument();
-});
-
